ab5e794620c2d5feca9bd235f0931a354aa5e6a8
lee-warrick <lee.warrick@capco.com>
2018-10-08 14:14:27 -0400

    add angular

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const app = express();
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, 'bizcard/dist/bizcard')));
 
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname, '/index.html'));
